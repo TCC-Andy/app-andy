23202c0307777b63eeeee71312f1210cafac6d98
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-02-25 21:45:20 -0300

    testando o cadastro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import api from './service/api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      nome: undefined,
+      sobrenome: undefined,
+    };
+  };
+
+  async handleSubmit(e){
+    e.preventDefault();
+    const data = {
+      nome: this.refs.nome.value,
+      sobrenome: this.refs.sobrenome.value,
+      status: 1
+    };    
+   await api.post('/usuarios', data).then((response) => {
+      if(response.data){
+        this.refs.nome.value = "";
+        this.refs.sobrenome.value = "";
+        return alert('usuario cadastrado com sucesso');
+      } else {
+        console.log('erro ao cadastrar usuario');
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="nome">Nome</label>
+            <input type="text" className="form-control" id="nome" ref="nome" placeholder="Nome"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="sobrenome">Sobrenome</label>
+            <input type="text" className="form-control" id="sobrenome" ref="sobrenome" placeholder="Sobrenome"/>
+          </div>
+          <button type="submit" className="btn btn-primary">Salvar</button>
+        </form>
+      </div>
+      )
+  }
 }
 
 export default App;
