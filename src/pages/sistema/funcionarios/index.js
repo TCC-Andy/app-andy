420675f0e0683518b0c27219a057a0aeef99c007
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-06-13 23:08:54 -0300

    quase finalizando o funcionario

--- src/pages/sistema/funcionarios/index.js
+++ src/pages/sistema/funcionarios/index.js
@@ -3,18 +3,129 @@ import api from '../../../service/api';
 import history from '../../../service/history';
 import Menu from '../menu/index';
 import '../styleGlobalSistema.css';
+import Select from 'react-select';
+import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 
 class Funcionario extends Component {
     constructor() {
         super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmitServices = this.handleSubmitServices.bind(this);
         this.state = {
-            logged: false
+            logged: false,
+            servicos: [],
+            servicosSelection: [],
+            idServicos: [],
+            nome: undefined,
+            sobrenome: undefined,
+            email: undefined,
+            telefone: undefined,
+            horaInicioTrabalho: undefined,
+            horaAlmocoInicio: undefined,
+            horaAlmocoFim: undefined,
+            horaFimTrabalho: undefined,
+            funcionarios: [],
+            editarFuncionarios: []
         }
     };
 
-    componentDidMount() {
+    componentDidMount = async () => {
         this.verifyToken();
+        this.loadServicos();
+        this.loadFuncionarios();
     };
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const data = {
+                nome: this.refs.nome.value,
+                sobrenome: this.refs.sobrenome.value,
+                email: this.refs.email.value,
+                telefone: this.refs.telefone.value,
+                idEmpresa: 1,
+                horaInicioTrabalho: this.refs.horaInicioTrabalho.value,
+                horaAlmocoInicio: this.refs.horaAlmocoInicio.value,
+                horaAlmocoFim: this.refs.horaAlmocoFim.value,
+                horaFimTrabalho: this.refs.horaFimTrabalho.value,
+                idServicos: this.state.idServicos,
+                servicosSelection: this.state.servicosSelection
+            }
+            if (!data.nome || !data.sobrenome || !data.email || !data.horaInicioTrabalho || !data.horaAlmocoInicio || !data.horaAlmocoFim || !data.horaFimTrabalho || !data.servicosSelection) {
+                this.setState({
+                    error: 'Favor preencher todos os campos.'
+                })
+            } else {
+                await api.post('/createEmp', data).then(response => {
+                    const carregaFuncao = this;
+                    if (response.data.status === 200) {
+                        this.setState({
+                            success: response.data.mensagem
+                        })
+                        this.loadFuncionarios();
+                        setTimeout(function () {
+                            carregaFuncao.limpaCadastroFuncionario();
+                        }, 1)
+                    } else {
+                        this.setState({
+                            error: response.data.mensagem
+                        });
+                    }
+                })
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    limpaCadastroFuncionario = async () => {
+        this.refs.nome.value = '';
+        this.refs.sobrenome.value = '';
+        this.refs.email.value = '';
+        this.refs.telefone.value = '';
+        this.refs.horaInicioTrabalho.value = '';
+        this.refs.horaAlmocoInicio.value = '';
+        this.refs.horaAlmocoFim.value = '';
+        this.refs.horaFimTrabalho.value = '';
+    }
+
+    handleSubmitServices = async (itens) => {
+        let idServicosSelection = [];
+        for (let i in itens) {
+            idServicosSelection.push(itens[i].value);
+        }
+        this.setState({
+            servicosSelection: itens,
+            idServicos: idServicosSelection
+        })
+    }
+
+    loadServicos = async () => {
+        const response = await api.get('/showServices/1');
+        let optionsServices = [];
+        for (let i in response.data.servicos) {
+            optionsServices.push({
+                value: response.data.servicos[i]._id,
+                label: response.data.servicos[i].nome
+            })
+        }
+        this.setState({ servicos: optionsServices });
+    }
+
+    loadFuncionarios = async () => {
+        const response = await api.get('/showEmps/1');
+        this.setState({ funcionarios: response.data.funcionarios });
+    }
+
+    excluirFuncionarios = async (id) => {
+        await api.delete(`/deleteEmp/${id}`);
+        this.loadFuncionarios();
+    }
+
+    editarFuncionarios = async (id) => {
+        const response = await api.get(`/showEmp/${id}`);
+        this.setState({ editarFuncionarios: response.data.func })
+    }
 
     async verifyToken() {
         const token = localStorage.getItem('Key_Andy');
@@ -47,14 +158,121 @@ class Funcionario extends Component {
     }
 
     render() {
+        const funcionarios = this.state.funcionarios;
+        const alterarFuncionarios = this.state.editarFuncionarios;
+        console.log(alterarFuncionarios);
         return (
+
             <div className="row">
                 <div className="col-md-2">
                     <Menu />
                 </div>
                 <div className="col-md-10">
                     <div className="corpo">
-                        <h1>Funcionario</h1>
+                        <div className="container">
+                            <h3 className="title">Funcionários</h3>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <form onSubmit={this.handleSubmit}>
+                                        <div className="form-row align-items-center justify-content-md-center">
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="servico">Nome</label>
+                                                <input type="text" className="form-control" id="nome" ref='nome' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.nome} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="descricao">Sobrenome</label>
+                                                <input type="text" className="form-control" id="sobrenome" ref='sobrenome' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.sobrenome} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="valor">E-mail</label>
+                                                <input type="email" className="form-control" id="email" ref='email' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.email} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="tempoEstimado">Telefone</label>
+                                                <input type="tel" className="form-control" id="telefone" ref='telefone' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.telefone} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="tempoEstimado">Hora Inicio Trabalho</label>
+                                                <input type="time" className="form-control" id="horaInicioTrabalho" ref='horaInicioTrabalho' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.horaInicioTrabalho} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="tempoEstimado">Hora Inicio Almoço</label>
+                                                <input type="time" className="form-control" id="horaAlmocoInicio" ref='horaAlmocoInicio' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.horaAlmocoInicio} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="tempoEstimado">Hora Fim Almoço</label>
+                                                <input type="time" className="form-control" id="horaAlmocoFim" ref='horaAlmocoFim' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.horaAlmocoFim} />
+                                            </div>
+                                            <div className="form-group col-md-6">
+                                                <label className="subTitulos" htmlFor="tempoEstimado">Hora Fim Trabalho</label>
+                                                <input type="time" className="form-control" id="horaFimTrabalho" ref='horaFimTrabalho' defaultValue={alterarFuncionarios.length === 0 ? '' : alterarFuncionarios.horaFimTrabalho} />
+                                            </div>
+                                            <div className="form-group col-md-12">
+                                                <label className="subTitulos" htmlFor="servicos">Serviços</label>
+                                                <Select
+                                                    defaultValue={alterarFuncionarios.length === 0 ? '' : console.log(alterarFuncionarios.servicosSelection.map((servicos, index) => [servicos[index]]))}
+                                                    isMulti
+                                                    //onChange={this.handleSubmitServices}
+                                                    name="serviços"
+                                                    options={this.state.servicos}
+                                                    className="basic-multi-select"
+                                                    placeholder="Selecione um e/ou mais Serviços."
+                                                    isSearchable
+                                                />
+                                            </div>
+                                        </div>
+                                        <button type="submit" className="btn btn-success botao">Cadastrar</button>
+                                        <button type="reset" className="btn btn-danger botao">Limpar</button>
+                                    </form>
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="table-responsive-md">
+                                <table className="table table-sm table-hover text-center align-middle">
+                                    <caption>Lista de Funcionários</caption>
+                                    <thead className="bgHead">
+                                        <tr>
+                                            <th>Nome</th>
+                                            <th>E-mail</th>
+                                            <th>Telefone</th>
+                                            <th>Inicio Trab.</th>
+                                            <th>Inicio Almoço.</th>
+                                            <th>Fim Almoço.</th>
+                                            <th>Fim Trab.</th>
+                                            <th>Serviços</th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {funcionarios.length === 0 ?
+                                            (
+                                                <tr>
+                                                    <th>Não existe funcionários cadastrado.</th>
+                                                </tr>
+                                            )
+                                            :
+                                            (
+                                                funcionarios.map(funcionarios => (
+                                                    <tr key={funcionarios._id}>
+                                                        <td className="align-middle">{funcionarios.nome + " " + funcionarios.sobrenome}</td>
+                                                        <td className="align-middle">{funcionarios.email}</td>
+                                                        <td className="align-middle">{funcionarios.telefone}</td>
+                                                        <td className="align-middle">{funcionarios.horaInicioTrabalho}</td>
+                                                        <td className="align-middle">{funcionarios.horaAlmocoInicio}</td>
+                                                        <td className="align-middle">{funcionarios.horaAlmocoFim}</td>
+                                                        <td className="align-middle">{funcionarios.horaFimTrabalho}</td>
+                                                        <td className="align-middle">{funcionarios.servicosSelection.map((servicos) => servicos.label).join(', ')}</td>
+                                                        <td className="text-primary pencilTrash align-middle" onClick={() => this.editarFuncionarios(funcionarios._id)}><FaPencilAlt /></td>
+                                                        <td className="text-danger pencilTrash align-middle" onClick={() => this.excluirFuncionarios(funcionarios._id)}><FaTrash /></td>
+                                                    </tr>
+                                                ))
+                                            )
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
